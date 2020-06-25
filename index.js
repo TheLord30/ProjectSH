621497d221cfaa8cfd3035e9b2b8d9d8b57d96b8
TheLord <adamvili75@gmail.com>
2020-06-25 18:00:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,6 @@ var embed = new Discord.MessageEmbed()
 SH.on("message", message => {
   if (message.content == "!register") {
     if(message.member.roles.cache.some(role => role.name === 'BetaCivi')) return message.channel.send(`${message.author} **You cant use this command beacuse you are already whitelisted**`);
-    message.delete(8000)
     message.channel.send(`**Hey ${message.author} I Have sent you a  Direct Message with  the registration instructions👍**`)
     message.author.send(embed)
   }
@@ -60,7 +59,6 @@ var embed1 = new Discord.MessageEmbed()
 SH.on("message", message => {
   if (message.content == "!mods") {
     if(message.member.roles.cache.some(role => role.name === 'player')) return message.channel.send(`${message.author} **You cant use this command beacuse you are Not whitelisted on this server Please use the !register command**`);
-    message.delete(8000)
     message.channel.send(`**Hey ${message.author} I have sent you a Direct Message with server mods instructions✅**`)
     message.author.send(embed1)
   }
